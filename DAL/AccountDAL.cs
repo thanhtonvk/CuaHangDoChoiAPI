3462cb6179fceb533bfb865f3ecc4467c78826d9
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-06-04 09:14:48 +0700

    updpate

---
+++ DAL/AccountDAL.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using CuaHangDoChoiAPI.Models;
+using CuaHangDoChoiAPI.Utils;
+using Microsoft.Data.SqlClient;
+
+namespace CuaHangDoChoiAPI.DAL
+{
+    public class AccountDAL
+    {
+        public int Register(string Username, string password)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@username", Username),
+                new SqlParameter("@password", password)
+            };
+            int rs = DBHelper.NonQuery("register", parameters);
+            return rs;
+        }
+
+        public Account Login(string username, string password)
+        {
+            SqlParameter[] parameters = new SqlParameter[]
+            {
+                new SqlParameter("@username", username),
+                new SqlParameter("@password", password)
+            };
+            Account account = null;
+            DataTable table = DBHelper.Query("login", parameters);
+            foreach (DataRow row in table.Rows)
+            {
+                account = new Account()
+                {
+                    UserName = row["UserName"].ToString(),
+                    Password = row["Password"].ToString()
+                };
+            }
+
+            return account;
+        }
+    }
+}
